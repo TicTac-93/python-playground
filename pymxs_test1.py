c828632ba7831911110c2486c45dd9f0d7e7d7b1
jhollander <joshh123@gmail.com>
2018-09-21 18:07:44 -0400

    Started experimenting with MaxScript execution.
    Added code to get a count of layers in current scene, and count + list of hidden layers.

--- pymxs_test1.py
+++ pymxs_test1.py
@@ -1,7 +1,36 @@
-# Import PyMXS and bind to var rt
+# Import PyMXS, MaxPlus, and set up shorthand vars
 import pymxs
+import MaxPlus
 
+# PyMXS variable setup
 rt = pymxs.runtime
-maxOut = rt.print_()
 
-maxOut('Hello, Max!')
+# MaxPlus variable setup
+maxScript = MaxPlus.Core.EvalMAXScript
+
+
+# Utility Functions
+def max_out(x):
+
+    """
+    Print x to MAXScript Listener.
+    """
+
+    output = 'print "' + str(x) + '"'  # Always convert to string, to be safe
+    maxScript(output)
+
+
+# Stuff
+max_out('Hello, Max!')
+
+layerCount = rt.layerManager.count
+hiddenLayers = []
+# Iterate over Max layers, append name of hidden layers to hiddenLayers[]
+for i in range(layerCount-1):
+    if not rt.layerManager.getLayer(i).on:
+        hiddenLayers.append(str(rt.LayerManager.getLayer(i).name))
+
+
+max_out('Number of Layers in current scene: ' + str(layerCount))
+max_out('Number of Hidden Layers in current scene: ' + str(len(hiddenLayers)))
+max_out(hiddenLayers)
