f56c094f1548399752407e10ef8b89d715e6361d
jhollander <joshh123@gmail.com>
2018-09-24 18:22:31 -0400

    Continued experimentation with pyMXS API.

--- pymxs_test1.py
+++ pymxs_test1.py
@@ -23,14 +23,48 @@ def max_out(x):
 # Stuff
 max_out('Hello, Max!')
 
+# ----------------
+# Layer Operations
+# ----------------
 layerCount = rt.layerManager.count
 hiddenLayers = []
+
 # Iterate over Max layers, append name of hidden layers to hiddenLayers[]
 for i in range(layerCount-1):
     if not rt.layerManager.getLayer(i).on:
         hiddenLayers.append(str(rt.LayerManager.getLayer(i).name))
 
 
+max_out('----- Layer Info -----')
 max_out('Number of Layers in current scene: ' + str(layerCount))
 max_out('Number of Hidden Layers in current scene: ' + str(len(hiddenLayers)))
 max_out(hiddenLayers)
+
+# Examples of doing stuff with layers, given a list of layer names
+# for l in hiddenLayers:
+#    tgtLayer = rt.LayerManager.getLayerFromName(l)  # Accesses a layer using its name
+#    tgtLayer.setParent(rt.LayerManager.getLayer(0))  # Sets layer parent to 0 (default)
+#    tgtLayer.on = True  # Sets the layer to be 'on', aka unhidden
+
+
+# ----------------
+# Light Operations
+# ----------------
+lightCount = rt.lights.count  # Max has several baked-in selection sets, such as lights, helpers, objects, etc.
+lightsOn = []
+lightsOff = []
+
+max_out('----- Light Info -----')
+max_out('Number of Lights in current scene: ' + str(rt.lights.count))
+
+# Iterate over all lights, print their baseObject, name
+# for i in rt.lights:
+#    max_out(str(i.baseObject) + ' -- ' + str(i.name))
+
+# Print all properties of first light
+max_out('----- Properties of ' + str(rt.lights[0].name) + ' -----')
+lightProperties = rt.getPropNames(rt.lights[0])
+for i in lightProperties:
+    max_out(i)
+
+max_out('----- End Properties -----')
