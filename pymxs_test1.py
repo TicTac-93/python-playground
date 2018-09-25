a38750f50e9c324bb91b3b6f9d407a2aacf73854
jhollander <joshh123@gmail.com>
2018-09-25 11:37:10 -0400

    Added function to build a dictionary of object properties, as well as some utility functions.

--- pymxs_test1.py
+++ pymxs_test1.py
@@ -11,13 +11,53 @@ maxScript = MaxPlus.Core.EvalMAXScript
 
 # Utility Functions
 def max_out(x):
-
     """
     Print x to MAXScript Listener.
     """
 
     output = 'print "' + str(x) + '"'  # Always convert to string, to be safe
     maxScript(output)
+
+
+def get_obj_props(obj):
+    """
+    Does what showProperties should do, and builds a dictionary of property:value pairs
+    for a valid Max object.
+    :param obj: The max object.
+    :return: A dictionary of property:value pairs.
+    """
+    propList = rt.getPropNames(obj)
+    propDict = {}
+
+    for i in propList:
+        propDict[i] = str(rt.getProperty(obj, i))
+
+    return propDict
+
+
+def pad_string(str1, str2, padding, x):
+    """
+    Concatenates two strings, padding the joint with whitespace so the second string
+    begins at a certain position.  Pads with one space if it's already past that pos.
+    :param str1: The first string
+    :param str2: The second string
+    :param padding: The minimum position for str2 to begin
+    :param x: The character to pad with.  Defaults to ' '
+    :return:
+    """
+    if x is None:
+        x = ' '
+
+    output = str1
+    padding = padding - len(str1)
+
+    if padding > 0:
+        output = output + (x * padding)
+    else:
+        output = output + x
+
+    output = output + str2
+    return output
 
 
 # Stuff
@@ -63,8 +103,8 @@ max_out('Number of Lights in current scene: ' + str(rt.lights.count))
 
 # Print all properties of first light
 max_out('----- Properties of ' + str(rt.lights[0].name) + ' -----')
-lightProperties = rt.getPropNames(rt.lights[0])
-for i in lightProperties:
-    max_out(i)
+lightProps = get_obj_props(rt.lights[0])
+for i in lightProps:
+    max_out(pad_string(str(i), str(rt.getProperty(rt.lights[0], i)), 25, '-'))
 
 max_out('----- End Properties -----')
