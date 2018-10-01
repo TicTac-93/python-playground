bcf7e9cebf2bad4116a757f92756a57438072ae8
jhollander <joshh123@gmail.com>
2018-10-01 15:10:49 -0400

    Added test for generating XML data

--- etree_test.py
+++ etree_test.py
@@ -30,3 +30,19 @@ for country in root.iter('country'):
     name = country.get('name')  # Get the 'name' attribute of the country element
     rank = country.find('rank').text  # Find the first child element named 'rank', and get its text value
     print name, rank
+
+print"""
+=======================
+Working with Custom XML
+=======================
+"""
+
+# Create a new tree
+myTree = ET.ElementTree(ET.Element('root'))
+# Get the root (aka, only node in the tree)
+myRoot = myTree.getroot()
+# Create a sub-element, using the ElementTree method
+ET.SubElement(myRoot, 'subEl-1', {'attrib1': 'A', 'attrib2': 'B', 'attrib3': 'C'})
+ET.SubElement(myRoot, 'subEl-2', {'attrib1': 'D', 'attrib2': 'E', 'attrib3': 'F'})
+for el in myRoot.iter():
+    print el.tag, el.attrib
