9facf7687ca0365b14c5e2496768062a6852e54d
jhollander <joshh123@gmail.com>
2018-10-02 12:53:40 -0400

    Added function to format XML data to be more human readable.  Uses a modified version of effbot's prettyprint function.  This will require some more work to flesh it out, and ideally it would not run in-place but instead return a duplicate XML tree.

--- etree_test.py
+++ etree_test.py
@@ -37,12 +37,44 @@ Working with Custom XML
 =======================
 """
 
+
+# Use element.text and element.tail to apply indents and newlines
+def xml_indent(el, depth=0, careful=False):
+    """
+    Formats an XML ETree with newlines and indents.
+    By default, assumes that nothing is stored in el.text or el.tail.
+    :param el: Root element of XML tree.
+    :param depth: Used for recursive calls, stores depth in tree.
+    :param careful: Checks for content in el.text and el.tail before overwriting.
+    :return: None - Operates on existing tree.
+    """
+    # Prep newline and indent for child elements
+    i = "\n" + depth*"\t"
+
+    # Check if we're being careful or not, switch accordingly
+    # If careful, append indent to contents of el.text and el.tail
+
+    # If careless, replace contents
+    if not careful:
+        if len(el):
+            el.text = i + "\t"  # Newline + Indent sub-elements
+            el.tail = i  # Add newline after closing tag
+            for el in el:  # Use same variable so that the last element in loop will remain accessible
+                xml_indent(el, depth + 1)
+            el.tail = i  # De-indent closing tag
+        else:  # If there aren't sub-elements, just add a newline
+            if not el.tail or not el.tail.strip():
+                el.tail = i
+
+
 # Create a new tree
 myTree = ET.ElementTree(ET.Element('root'))
 # Get the root (aka, only node in the tree)
 myRoot = myTree.getroot()
-# Create a sub-element, using the ElementTree method
-ET.SubElement(myRoot, 'subEl-1', {'attrib1': 'A', 'attrib2': 'B', 'attrib3': 'C'})
-ET.SubElement(myRoot, 'subEl-2', {'attrib1': 'D', 'attrib2': 'E', 'attrib3': 'F'})
-for el in myRoot.iter():
-    print el.tag, el.attrib
+# Create sub-elements, using the ElementTree method
+subEl1 = ET.SubElement(myRoot, 'subEl-1', {'attrib1': 'A', 'attrib2': 'B', 'attrib3': 'C'})
+subEl2 = ET.SubElement(myRoot, 'subEl-2', {'attrib1': 'D', 'attrib2': 'E', 'attrib3': 'F'})
+ET.SubElement(subEl1, 'subEl-1a', {'foo': 'bar', 'blah': 'blub'})
+
+xml_indent(myRoot)
+myTree.write('myXML.xml')
